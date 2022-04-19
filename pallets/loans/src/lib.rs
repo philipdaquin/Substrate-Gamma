a15865874f9d84ef3b529ee518c08f25df375c00
philipdaquin <philipdaquin20@gmail.com>
2022-04-19 20:35:24 +0800

    objects

--- pallets/loans/src/lib.rs
+++ pallets/loans/src/lib.rs
@@ -6,6 +6,7 @@
 pub use pallet::*;
 use common::{MultiAsset, Oracle};
 mod model;
+use model::Pool;
 
 // #[cfg(test)]
 // mod mock;
@@ -18,11 +19,12 @@ mod model;
 pub mod pallet {
 	use super::*;
 	use codec::{HasCompact, MaxEncodedLen};
-	use frame_support::pallet_prelude::{*, Member};
+	use frame_support::{pallet_prelude::{*, Member}, Blake2_128Concat};
 	use frame_system::pallet_prelude::*;
 	use sp_runtime::{traits::AtLeast32BitUnsigned, FixedU128};
-
-
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
@@ -38,27 +40,33 @@ pub mod pallet {
 		///	MultiAsset Transfer
 		type MultiAsset: MultiAsset<Self::AccountId, Self::AssetID, Self::Balance>;
 	}
-
-	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
-	pub struct Pallet<T>(_);
-
+	
 	// The pallet's runtime storage items.
 	// https://docs.substrate.io/v3/runtime/storage
 	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
+	#[pallet::getter(fn get_pool_info)]
+	pub type PoolInfo<T: Config> = StorageMap<
+		_, 
+		 Twox64Concat, 
+		T::AssetID, 
+		Pool<T>,
+		ValueQuery
+	>;
 
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/v3/runtime/events-and-errors
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored(u32, T::AccountId),
+		/// Supplied Assets to a pool[asset_id]
+		Supplied { asset_id: T::AssetID, supplied_amount: T::Balance, account_id: T::AccountId },
+		///	Borrowed Assets from a pool[asset_id]
+		Borrowed { asset_id: T::AssetID, borrowed_amoutn: T::Balance, account_id: T::AccountId},
+		///	Withdrawn Assets from a pool
+		Withdrawn { asset_id: T::AssetID, withdrawn_amount: T::Balance, account_id: T::AccountId },
+		///	Repaid assets to a pool
+		Repaid { asset_id: T::AssetID, repaid_amount: T::Balance, account_id: T::AccountId },
+		// Liquidated {}
 	}
 
 	// Errors inform users that something went wrong.
--- pallets/loans/src/model.rs
+++ pallets/loans/src/model.rs
@@ -2,26 +2,26 @@ use super::*;
 use codec::{Decode, Encode};
 use frame_support::RuntimeDebug;
 use sp_runtime::{FixedU128, FixedI128};
+use super::Config;
 
-
-#[derive(Clone, PartialEq, Eq, RuntimeDebug, Encode, Decode)]
-pub struct Pool<Balance, AssetID, BlockNumber> { 
+#[derive(Clone, PartialEq,  RuntimeDebug, Encode, Decode)]
+pub struct Pool<T: Config> { 
     /// If the pool is enabled
     pub enabled: bool,
     /// If the asset can be enabled as collateral
     pub is_collateral: bool,
     /// The underlying asset
-    pub asset: AssetID,
+    pub asset: T::AssetID,
     /// Total supply of the pool 
-    pub total_supply: Balance,
+    pub total_supply: T::Balance,
     /// Total debt of the pool
-    pub debt: Balance,
+    pub debt: T::Balance,
     /// Effective index of current total supply
     pub total_supply_index: FixedU128,
     /// Effective index of current total debt
     pub total_debt_index: FixedU128,
     /// The latest timestamp that the pool has accrued interest
-    pub last_updated: BlockNumber,
+    pub last_updated: T::BlockNumber,
     /// One factor of the linear interest model 
     pub utilization_factor: FixedU128,
     /// Another factor of the linear interest model
@@ -29,8 +29,24 @@ pub struct Pool<Balance, AssetID, BlockNumber> {
     /// A discount factor for an asset that reduces the its limit 
     pub safe_factor: FixedU128, 
     /// Factor that determines what percentage one arbitrage can seize <= 1
-    pub close_facor: FixedU128,
+    pub close_factor: FixedU128,
     /// The bonus arbitrager can get when triggering a liquidation 
     pub discount_factor: FixedU128
 }
 
+/// Information about the User Supplied Assets
+#[derive(PartialEq, RuntimeDebug, Encode, Decode, Clone)]
+pub struct UserAssets<T: Config> { 
+    /// Asset Id Supplied
+    pub asset_id: T::AssetID,
+    /// Amount supplied by the User
+    pub supplied_amount: T::Balance
+}
+/// Information about the User Debt
+#[derive(Clone, PartialEq, RuntimeDebug, Encode, Decode)]
+pub struct UserDebt<T: Config> { 
+    /// AssetId Owed
+    pub asset_id: T::AssetID, 
+    /// Debt accounted by the User
+    pub debt_amount: T::Balance
+}
